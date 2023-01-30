c795a411bcb1eb43ee25bc5c54ecbb1c0cb79e8e
Malek Omri <momri@stu.jsu.edu>
2023-01-29 23:12:33 -0600

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,9 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        StringBuilder input = new StringBuilder(message);
+
+        return input.reverse().toString();
     }
     
 }
